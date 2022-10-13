161eca1c51d1cb2c798dfb039ec44c9987a05e53
wagnerfe <wagner@mcc-berlin.net>
2022-10-13 09:29:47 +0200

    updates distace to center and subcenter calculation and check for right crs in graph

--- ufo_map/Feature_engineering/city_level.py
+++ ufo_map/Feature_engineering/city_level.py
@@ -51,7 +51,8 @@ def distance_cbd(gdf, gdf_loc):
     return gdf
 
 
-def distance_cbd_shortest_dist(gdf, gdf_loc, ox_graph):
+
+def distance_cbd_shortest_dist(gdf, gdf_loc, ox_graph, col_name):
     """
     Returns a DataFrame with an additional line that contains the distance to a given point
     based on the shortest path calculated with igraph's shortest_path function.
@@ -84,30 +85,28 @@ def distance_cbd_shortest_dist(gdf, gdf_loc, ox_graph):
     # then we have to convert the multigraph object to a dataframe
     gdf_nodes, gdf_edges = ox.utils_graph.graph_to_gdfs(ox_graph)
 
-    # call nearest neighbour function
+    # call nearest neighbour functions
     gdf_orig = nearest_neighbour(gdf, gdf_nodes)
     gdf_dest = nearest_neighbour(gdf_loc, gdf_nodes)
 
     graph_ig, list_osmids = convert_to_igraph(ox_graph)
-    gdf['feature_distance_cbd'] = gdf_orig.apply(lambda x: get_shortest_dist(graph_ig,
-                                                                                  list_osmids,
-                                                                                  x.osmid,
-                                                                                  gdf_dest.osmid.iloc[0],
-                                                                                  'length'),
-                                                                                    axis=1)
+    gdf[col_name] = gdf_orig.apply(lambda x: get_shortest_dist(graph_ig,
+                                                                list_osmids,
+                                                                x.osmid,
+                                                                gdf_dest.osmid.iloc[0],
+                                                                'length'),
+                                                                axis=1)
 
     # add distance from hex center to nearest node (only for nodes where distance != inf)
-    dist_start = gdf_orig['distance'][gdf.feature_distance_cbd != np.inf]
+    dist_start = gdf_orig['distance'][gdf[col_name] != np.inf]
     dist_end = gdf_dest['distance'][0]
-    gdf.loc[gdf.feature_distance_cbd != np.inf,'feature_distance_cbd'] += dist_start + dist_end
+    gdf.loc[gdf[col_name] != np.inf,col_name] += dist_start + dist_end
 
     # check for nodes that could not be connected
     # create numpy array
-    np_geom = gdf.geometry[gdf.feature_distance_cbd == np.inf].values
+    np_geom = gdf.geometry[gdf[col_name] == np.inf].values
     # assign distance to cbd array
-    gdf.loc[gdf.feature_distance_cbd == np.inf,'feature_distance_cbd'] = np_geom[:].distance(gdf_loc.geometry.iloc[0])
-
-    print('Calculated distance to cbd based on shortest path')
+    gdf.loc[gdf[col_name] == np.inf,col_name] = np_geom[:].distance(gdf_loc.geometry.iloc[0])
     return gdf
 
 
@@ -132,7 +131,7 @@ def distance_local_cbd(gdf, gdf_loc_local):
     return gdf_out
 
 
-def distance_local_cbd_shortest_dist(gdf, gdf_loc_local, graph):
+def distance_local_cbd_shortest_dist(gdf, gdf_loc_local, ox_graph,col_name):
     """
     Returns a DataFrame with an additional line that contains the distance to points in gdf_loc_local
     based on the shortest path calculated with igraph's shortest_path function.
@@ -158,47 +157,39 @@ def distance_local_cbd_shortest_dist(gdf, gdf_loc_local, graph):
     Last update: 01/07/21. By Felix.
     """
 
-    # call nearest neighbour to find nearest local center
-    gdf_out = nearest_neighbour(gdf, gdf_loc_local)
-    # rename distance column
-    gdf_out = gdf_out.rename(columns={'distance': 'distance_crow'})
-    # remove unnecessary columns
-    gdf_out = gdf_out.drop(columns={'closeness_global', 'kiez_name'})
-
-    # convert input gdf to crs
-    gdf = gdf_out.to_crs(4326)
-    gdf_loc_local = gdf_loc_local.to_crs(4326)
-
-    # then we have to convert the multigraph object to a dataframe
-    gdf_nodes, gdf_edges = ox.utils_graph.graph_to_gdfs(graph)
-    # call nearest neighbour function to find nearest node
-    gdf_orig = nearest_neighbour(gdf, gdf_nodes)
+    if ox_graph.graph['crs'] != gdf.crs:
+        print('adjusting graph crs to local crs')
+        ox_graph = ox.project_graph(ox_graph, to_crs=gdf.crs)
+        
+    # find nearest local center 
+    gdf_tmp = nearest_neighbour(gdf, gdf_loc_local)
+    gdf_tmp = gdf_tmp.rename(columns={'distance': 'distance_crow'})
+    
+    # convert multigraph to df and find nearest nodes at orig and dest
+    gdf_nodes, gdf_edges = ox.utils_graph.graph_to_gdfs(ox_graph)
+    gdf_orig = nearest_neighbour(gdf_tmp, gdf_nodes)
     gdf_dest = nearest_neighbour(gdf_loc_local, gdf_nodes)
+    gdf_dest['index'] = gdf_dest.index
+    gdf_merge = gdf_orig.merge(gdf_dest, how='left', on=['index'])
 
-    # merge on node ID
-    gdf_merge = gdf_orig.merge(gdf_dest, how='left', on=['nodeID'])
-
-    # convert to igraph
-    graph_ig, list_osmids = convert_to_igraph(graph)
-
+    # transform to igraph for performance
+    graph_ig, list_osmids = convert_to_igraph(ox_graph)
     # call get shortest dist func, where gdf_merge_3426.osmid_x is nearest node from starting point and osmid_y is
     # nearest node from end destination (one of the neighbourhood centers)
-    gdf['feature_distance_local_cbd'] = gdf_merge.apply(lambda x: get_shortest_dist(graph_ig,
-                                                                                         list_osmids,
-                                                                                         x.osmid_x,
-                                                                                         x.osmid_y,
-                                                                                         'length'),
-                                                             axis=1)
+    gdf[col_name] = gdf_merge.apply(lambda x: get_shortest_dist(graph_ig,
+                                                                list_osmids,
+                                                                x.osmid_x,
+                                                                x.osmid_y,
+                                                                'length'),
+                                                                axis=1)
 
     # add distance from hex center to nearest node (only for nodes where distance != inf)
-    dist_start = gdf_merge['distance_x'][gdf.feature_distance_local_cbd != np.inf]
-    dist_end = gdf_merge['distance_y'][gdf.feature_distance_local_cbd != np.inf]
-    gdf.feature_distance_local_cbd[gdf.feature_distance_local_cbd != np.inf] += dist_start + dist_end
+    dist_start = gdf_merge['distance_x'][gdf[col_name] != np.inf]
+    dist_end = gdf_merge['distance_y'][gdf[col_name] != np.inf]
+    gdf[col_name][gdf[col_name] != np.inf] += dist_start + dist_end
 
     # check for nodes that could not be connected and assing crow flies distance
-    gdf.feature_distance_local_cbd[gdf.feature_distance_local_cbd ==
-                                   np.inf] = gdf_merge['distance_crow'][gdf.feature_distance_local_cbd == np.inf]
-
+    gdf[col_name][gdf[col_name]==np.inf] = gdf_merge['distance_crow'][gdf[col_name] == np.inf]
     print('Calculated distance to local cbd based on shortest path')
     return gdf
 
